1accfa8466a9d8810aa3d57f04faa73275638047
hariprasad <harih79562@gmail.com>
2024-08-14 12:08:45 +0530

    news added in home page

--- app/home/page.js
+++ app/home/page.js
@@ -10,6 +10,7 @@ import Link from "next/link";
 import InnerHeader from "../components/commen/InnerHeader/Header";
 import Footer from "../components/commen/Footer/Footer";
 import Loader from "../components/commen/Loader/Loader";
+import { useSession } from "next-auth/react";
 // Style
 import Styles from "./home.module.scss";
 import axios from "axios";
@@ -17,11 +18,14 @@ import axios from "axios";
 // Home component
 const Home = () => {
   const [imageUrl, setImageUrl] = useState(null);
+  const [apiResponse, setApiResponse] = useState({});
+  const session = useSession();
 
   // useEffect for triger fetch home page data function
   useEffect(() => {
     fetchImagUrlForHomePage();
-  }, []);
+    getData();
+  }, [session]);
 
   // Function for fetch image url
   async function fetchImagUrlForHomePage() {
@@ -32,6 +36,39 @@ const Home = () => {
       }
     } catch (error) {
       console.log("error ->", error.message);
+    }
+  }
+
+  // Function for API call to next server for news
+  async function getData() {
+    try {
+      let userId = session.data?.user?.id;
+      if (!userId) {
+        throw new Error("user id not found");
+      }
+
+      // API call
+      const response = await axios.post(`/api/news`, {
+        userId,
+        lastupdatedttm: "2012-03-12 05:49:32",
+        userguidelastupdatedttm: "2017-08-21 16:03:19",
+      });
+
+      // Setting data with status A in to a state
+      if (
+        response?.data?.result?.communication &&
+        response?.data?.result?.communication?.length !== 0
+      ) {
+        let communicationArray = response.data.result.communication;
+        for (let i = 0; i < communicationArray.length; i++) {
+          if (communicationArray[i]?.status == "A") {
+            setApiResponse(communicationArray[i]);
+            break;
+          }
+        }
+      }
+    } catch (error) {
+      console.log("error", error.message);
     }
   }
 
@@ -59,20 +96,11 @@ const Home = () => {
                   <div
                     className={`${Styles.fntw700} ${Styles.pdTB10}  ${Styles.subtitle} `}
                   >
-                    Visit Reports
+                    {apiResponse.title}
                   </div>
-                  <Link href="/read-more">
-                    <div
-                      className={`${Styles.fntclrd} ${Styles.pdB10} ${Styles.fnt13}`}
-                    >
-                      <button
-                        className={`${Styles.fntclrd} ${Styles.readmorebtn}`}
-                      >
-                        [Tap to read more]
-                      </button>
-                    </div>
-                  </Link>
-                  <div className={Styles.fntclgry}>15/07/2024</div>
+                  
+                  <div className={Styles.fntclgry}>{apiResponse.date}</div>
+                  <div>{apiResponse.desc}</div>
                 </div>
               </div>
             </div>
