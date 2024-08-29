bcc76a98727d1acbc7f13eb203d0ce80ca01d453
joijo93 <joaijo93@gmail.com>
2024-08-29 18:11:38 +0530

    Guide size changed

--- app/components/commen/InnerHeader/Header.js
+++ app/components/commen/InnerHeader/Header.js
@@ -228,6 +228,7 @@ function UserGuide({ show, setShow }) {
         onHide={() => setShow(false)}
         dialogClassName="modal-90w"
         aria-labelledby="example-custom-modal-styling-title"
+        className={Styles.userguidepopup}
       >
         <Modal.Header closeButton></Modal.Header>
         <Modal.Body>
